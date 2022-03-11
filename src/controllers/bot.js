5e8375cb6394045bd3f5db4cd9240c821e6ce40f
Austin <augustineumeagudosi@gmail.com>
2022-03-11 16:03:00 +0100

    changed messages format

--- src/controllers/bot.js
+++ src/controllers/bot.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export const greetings = async (req, res) => {
-  const welcomeMessagePath = path.join(__dirname, '..', 'utils', 'messages', 'welcome.json');
+  const welcomeMessagePath = path.join(__dirname, '..', 'utils', 'messages', 'welcome.txt');
   const data = fs.readFileSync(welcomeMessagePath, 'utf8');
   
   return res.json(data);
