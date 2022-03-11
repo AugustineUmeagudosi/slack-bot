a9d34a083cc60ab442fda9e7c14eff92a07ced35
Austin <augustineumeagudosi@gmail.com>
2022-03-11 21:38:37 +0100

    minor updates

--- src/controllers/bot.js
+++ src/controllers/bot.js
@@ -5,5 +5,5 @@ export const greetings = async (req, res) => {
   const welcomeMessagePath = path.join(__dirname, '..', 'utils', 'messages', 'welcome.txt');
   const data = fs.readFileSync(welcomeMessagePath, 'utf8');
   
-  return res.json(data);
+  return res.json(JSON.parse(data));
 };
